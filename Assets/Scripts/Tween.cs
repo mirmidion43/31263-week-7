0b9369171ed219cf3e9c8445a6223dd6b83ef118
mirmidion43 <69776783+mirmidion43@users.noreply.github.com>
2020-09-07 13:27:23 +1000

    50% done

---
+++ Assets/Scripts/Tween.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tween
+{
+    public Transform Target {get; private set;}
+    public Vector3 StartPos {get; private set;}
+    public Vector3 EndPos {get; private set;}
+    public float StartTime {get; private set;}
+    public float Duration {get; private set;}
+
+    
+}
--- Assets/Scripts/Tweener.cs
+++ Assets/Scripts/Tweener.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Tweener : MonoBehaviour
 {
+    private Tween activeTween;
     // Start is called before the first frame update
     void Start()
     {
